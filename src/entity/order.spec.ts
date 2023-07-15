16dcad799a867faec55350c8c125730b956df646
Eduardo Zaghi <eduardo@loldesign.com.br>
2023-07-15 16:38:58 -0300

    test: order entity

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -21,10 +21,17 @@ describe("Order", () => {
   })
 
   it("should calculate total", () => {
-    const item1 = new OrderItem("1", "Item 1", 10);
-    const item2 = new OrderItem("2", "Item 2", 20);
+    const item1 = new OrderItem("1", "Item 1", 10, "p1", 2);
+    const item2 = new OrderItem("2", "Item 2", 20, "p2", 2);
     const order = new Order("123", "123", [item1, item2]);
 
-    expect(order.total()).toBe(30);
+    expect(order.total()).toBe(60);
+  })
+
+  it("should check if the qte is greater than 0", () => {
+    expect(() => {
+      const item1 = new OrderItem("1", "Item 1", 10, "p1", 0);
+      const order = new Order("123", "123", [item1]);      
+    }).toThrow("Item qtd must be greater than 0");
   })
 })
--- src/entity/order_item.ts
+++ src/entity/order_item.ts
@@ -1,15 +1,41 @@
 export default class OrderItem {
-  _id: string;
-  _name: string;
-  _price: number;
+  private _id: string;
+  private _name: string;
+  private _price: number;
+  private _productId: string;
+  private _quantity: number;
+  private _total: number;
   
-  constructor(id: string, name: string, price: number) {
+  constructor(id: string, name: string, price: number, productId: string, quantity: number) {
     this._id = id;
     this._name = name;
     this._price = price;
+    this._productId = productId;
+    this._quantity = quantity;
+    this._total = this.total();
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
   }
 
   get price(): number {
     return this._price;
+  }
+
+  get productId(): string {
+    return this._productId;
+  }
+
+  get quantity(): number {
+    return this._quantity;
+  }
+
+  total(): number {
+    return this._quantity * this._price;
   }
 }
