2e57eb82699d5c99e10b9706c42a8e45ed4c94de
Eduardo Zaghi <eduardo@loldesign.com.br>
2023-07-15 16:44:26 -0300

    fix: OrderItem params on main

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ const address = new Address("Rua Visconde de Cairu", "134", "03110-040", "São P
 customer.Address = address;
 customer.activate();
 
-const item1 = new OrderItem("1", "Item 1", 10);
-const item2 = new OrderItem("2", "Item 2", 20);
+const item1 = new OrderItem("1", "Item 1", 10, "p1", 2);
+const item2 = new OrderItem("2", "Item 2", 20, "p2", 2);
 const order = new Order("1", customer.id, [item1, item2]);
