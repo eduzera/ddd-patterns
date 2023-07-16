53951b77697b9f63bdeb5cc4cc0c4f75519fb9c9
Eduardo Zaghi <eduardo@loldesign.com.br>
2023-07-16 10:02:03 -0300

    feat: customer repository

---
+++ src/infrastructure/db/sequelize/model/customer.model.ts
@@ -0,0 +1,33 @@
+import {Model, Table, PrimaryKey, Column} from "sequelize-typescript";
+
+@Table({
+  timestamps: false,
+  tableName: 'customers'
+})
+
+export default class CustomerModel extends Model {
+  @PrimaryKey
+  @Column
+  declare id: string;
+
+  @Column({ allowNull: false })
+  declare name: string;
+
+  @Column({ allowNull: false })
+  declare street: string;
+
+  @Column({ allowNull: false })
+  declare number: number;
+
+  @Column({ allowNull: false })
+  declare zipcode: string;
+
+  @Column({ allowNull: false })
+  declare city: string;
+
+  @Column({ allowNull: false })
+  declare active: boolean;
+
+  @Column({ allowNull: false })
+  declare rewardPoints: number;
+}
