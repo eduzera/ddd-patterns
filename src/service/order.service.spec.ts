00963142c7788a580217e1fb30c8208a8a00cdaa
Eduardo Zaghi <eduardo@loldesign.com.br>
2023-07-16 08:42:32 -0300

    feat: add placeOrder to OrderService

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,8 +1,18 @@
+import Customer from "../entity/customer";
 import Order from "../entity/order";
 import OrderItem from "../entity/order_item";
 import OrderService from "./order.service";
 
 describe("Order Service", () => {
+  it("should place an order", () => {
+    const customer = new Customer("c1", "Edu Zaghi");
+    const item1 = new OrderItem("i1", "Item 1", 10, "p1", 1);
+
+    const order = OrderService.placeOrder(customer, [item1]);
+
+    expect(customer.rewardsPoints).toBe(5);
+  })
+
   it("should get total of all orders", () => {
     const item1 = new OrderItem("1", "Item 1", 100, "p1", 1);
     const item2 = new OrderItem("2", "Item 2", 200, "p2", 2);
--- src/service/order.service.ts
+++ src/service/order.service.ts
@@ -1,7 +1,20 @@
+import Customer from "../entity/customer";
 import Order from "../entity/order";
 import OrderItem from "../entity/order_item";
+import {v4 as uuid} from "uuid";
 
 export default class OrderService {
+
+  static placeOrder(customer: Customer, items: OrderItem[]): Order {
+    if(items.length === 0) {
+      throw new Error("Items are required");
+    }
+
+    const order = new Order(uuid(), customer.id, items);
+    customer.addRewardsPoints(order.total() / 2);
+
+    return order
+  }
   static total(orders: Order[]): number {
     return orders.reduce((acc, order) => acc + order.total(), 0);
   }
